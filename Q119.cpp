dcc5858ccf5349d231bffaf44f290e76436991e9
singhshreyash75-eng <singhshreyash75@gmail.com>
2025-11-25 19:31:52 +0530

    Q119.cpp

---
+++ Q119.cpp
@@ -0,0 +1,28 @@
+//119) Write a program to take an input array of size n. The array should contain all the integers between 0 to n except for one. Print that missing number
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);      // size of array
+    
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Expected sum of numbers from 0 to n
+    int expected_sum = n * (n + 1) / 2;
+
+    // Actual sum of array elements
+    int actual_sum = 0;
+    for(int i = 0; i < n; i++) {
+        actual_sum += arr[i];
+    }
+
+    // Missing number
+    int missing = expected_sum - actual_sum;
+
+    printf("%d", missing);
+
+    return 0;
+}
